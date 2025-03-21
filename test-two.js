c4d8841ec541ba1478b9bbb152a7bc98ef73abb3
Narcis Radu <github@narcisradu.ro>
2025-03-21 14:36:06 +0200

    Create test-two.js

---
+++ test-two.js
@@ -0,0 +1,3 @@
+const paragraph = document.querySelector('body p');
+paragraph?.innerText = 'Top Cat! The most effectual Top Cat! Who’s intellectual close friends get to call him T.C., providing it’s with dignity. Top Cat! The indisputable leader of the gang. He’s the boss, he’s a pip, he’s the championship. He’s the most tip top, Top Cat.';
+paragraph?.style.display = 'block';
